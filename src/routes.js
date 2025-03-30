22a0dfaab2befbeee6da8efce3760bcc7e9f2546
Usman Chaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2025-03-30 04:17:59 -0700

    New QrCode page

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const Trainers = React.lazy(() => import('./views/trainer/Trainer.js'))
 const Login = React.lazy(() => import('./views/pages/login/Login.js'))
 const Register = React.lazy(() => import('./views/pages/register/Register.js'))
 const Membership = React.lazy(() => import('./views/membership/Membership.js'))
-
+const QRCodePage = React.lazy(() => import('./views/pages/qrcode/QRCodePage.js'))
 // NEW: Lazy import the ConsentForm
 const ConsentForm = React.lazy(() => import('./views/consent/ConsentForm.js'))
 
@@ -19,6 +19,8 @@ const routes = [
   { path: '/register', name: 'Register', element: Register },
   { path: '/memberships', name: 'Membership', element: Membership },
   { path: '/consent', name: 'Consent', element: ConsentForm },
+  { path: '/qrcode', name: 'QR Code', element: QRCodePage },
+
 ]
 
 export default routes
